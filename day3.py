570c940e3e1bcddc81c9f3623fbe7e98207717bd
Anthony Holloway <avholloway@gmail.com>
2021-01-03 19:38:09 +0000

    simplified the if

--- day3.py
+++ day3.py
@@ -25,11 +25,11 @@ print(f"Your BMI is {bmi}, ", end="")
 
 if bmi < 18.5:
   print("you are underweight.")
-elif bmi >= 18.5 and bmi < 25:
+elif bmi < 25:
   print("you have a normal weight.")
-elif bmi >= 25 and bmi < 30:
+elif bmi < 30:
   print("you are slightly overweight.")
-elif bmi >= 30 and bmi < 35:
+elif bmi < 35:
   print("you are obese.")
 else:
   print("you are clinically obese.")
