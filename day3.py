0ef073867c30903f2260d4c2bd7dd334a89da21f
Anthony Holloway <avholloway@gmail.com>
2021-01-03 18:54:33 +0000

    added 3.1

--- day3.py
+++ day3.py
@@ -1,6 +1,9 @@
+# 3.1 - Even or odd
+# ---------------------------------------------------------
 # 🚨 Don't change the code below 👇
 number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
 
+print(f"This is an {'even' if (number % 2 == 0) else 'odd'} number")
