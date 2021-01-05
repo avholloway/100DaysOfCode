26bc82e6ddd376a7b2d4ae2766793b3616b6c96f
Anthony Holloway <avholloway@gmail.com>
2021-01-05 05:37:35 +0000

    4.3 done

--- day4.py
+++ day4.py
@@ -18,14 +18,42 @@ def one():
 # 4.2 - who pays?
 # ---------------------------------------
 # Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
+def two():
+  names_string = input("Give me everybody's names, separated by a comma. ")
+  names = names_string.split(", ")
+  # 🚨 Don't change the code above 👆
+
+  #Write your code below this line 👇
+
+  from random import randint
+
+  who = randint(0, len(names) - 1)
+
+  print(f"{names[who]} pays")
+
+# 4.3 - treasure map
+# ---------------------------------------
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+#Write your code below this row 👇
 
-from random import randint
+# take 2-digit input as string
+# split each digit into own list element
+# converting each to int
+# and subtract 1 from each for 0-indexing
+col, row = [int(p) - 1 for p in list(position)]
 
-who = randint(0, len(names) - 1)
+# place marker on map
+map[row][col] = "X"
 
-print(f"{names[who]} pays")
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
