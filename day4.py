d65185211910b0fd8beb93f1564b23b3d0bf94d1
Anthony Holloway <avholloway@gmail.com>
2021-01-05 05:21:00 +0000

    4.2 done

--- day4.py
+++ day4.py
@@ -2,16 +2,30 @@
 # ---------------------------------------
 # Write your code below this line 👇
 # Hint: Remember to import the random module first. 🎲
+def one():
+  from random import randint
+
+  heads = tails = 0
+  # flip 1 million coins and generate a histogram of the outcomes
+  for _ in range(1_000_000):
+    r = randint(0, 1)
+    heads += r     # increment by r
+    tails += r ^ 1 # increment by the opposite of r
+
+  print(f"Heads ({heads}) {'○' * (heads//10_000)}")
+  print(f"Tails ({tails}) {'●' * (tails//10_000)}")
+
+# 4.2 - who pays?
+# ---------------------------------------
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
 
 from random import randint
 
-heads = tails = 0
-# flip 1 million coins and generate a histogram of the outcomes
-for _ in range(1_000_000):
-  r = randint(0, 1)
-  heads += r     # increment by r
-  tails += r ^ 1 # increment by the opposite of r
+who = randint(0, len(names))
 
-print(f"Heads ({heads}) {'○' * (heads//10_000)}")
-print(f"Tails ({tails}) {'●' * (tails//10_000)}")
-
+print(f"{names[who]} pays")
