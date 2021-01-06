f8d4a47da7c3da8b6f977e73ee84abfc3861e935
Anthony Holloway <avholloway@gmail.com>
2021-01-06 00:46:53 +0000

    5.3 and 5.4 done

--- day5.py
+++ day5.py
@@ -20,17 +20,45 @@ def one():
 
 # 5.2 - highest score
 # --------------------------------------
-# 🚨 Don't change the code below 👇
-student_scores = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-print(student_scores)
-# 🚨 Don't change the code above 👆
+def two():
+  # 🚨 Don't change the code below 👇
+  student_scores = input("Input a list of student scores ").split()
+  for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+  print(student_scores)
+  # 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
+  #Write your code below this row 👇
 
-high_score = 0
-for score in student_scores:
-  if score > high_score:
-    high_score = score
-print(f"The highest score in the class is: {high_score}")
+  high_score = 0
+  for score in student_scores:
+    if score > high_score:
+      high_score = score
+  print(f"The highest score in the class is: {high_score}")
+
+# 5.3 - adding evens
+# --------------------------------------
+def three():
+  #Write your code below this row 👇
+
+  total = 0
+  for i in range(1, 101):
+    if i % 2 == 0:
+      total += i
+  print(total)
+
+# 5.4 - fizzbuzz
+# --------------------------------------
+def four():
+  #Write your code below this row 👇
+
+  for i in range(1, 101):
+    if i % 15 == 0:
+      print("FizzBuzz")
+    elif i % 3 == 0:
+      print("Fizz")
+    elif i % 5 == 0:
+      print("Buzz")
+    else:
+      print(i)
+
