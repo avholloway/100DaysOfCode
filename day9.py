6bf729c6eecd318729ca2f62969993c0b1cd1526
Anthony Holloway <avholloway@gmail.com>
2021-01-09 19:49:01 +0000

    day 9 complete

--- day9.py
+++ day9.py
@@ -38,3 +38,68 @@ def one():
 
 # 9.2 - travel log
 # -----------------------------------------------------------
+def two():
+    travel_log = [
+    {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+    ]
+    #🚨 Do NOT change the code above
+
+    #TODO: Write the function that will allow new countries
+    #to be added to the travel_log. 👇
+
+    def add_new_country(country, visits, cities):
+        travel_log.append({
+            "country": country,
+            "visits": visits,
+            "cities": cities
+        })
+
+
+    #🚨 Do not change the code below
+    add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+    print(travel_log)
+
+# final - auction
+# -----------------------------------------------------------
+from replit import clear
+from auctionart import logo
+#HINT: You can call clear() to clear the output in the console.
+
+print(logo)
+print("Welcome to the secret auction program!")
+print()
+
+bids = {}
+
+def collect_bidder_info():
+    name = input("Name: ")
+    bid = int(input("Bid: $"))
+    bids[name] = bid
+
+collect_bidder_info()
+while input("Another (yes/no): ") == "yes":
+    clear()
+    collect_bidder_info()
+
+winning = {
+    "name": "no one",
+    "bid": 0
+}
+
+for name in bids:
+    this_bid = bids[name]
+    if this_bid > winning["bid"]:
+        winning["name"] = name
+        winning["bid"] = this_bid
+
+clear()
+print(f"The winner is {winning['name']} with a bid of {winning['bid']}")
