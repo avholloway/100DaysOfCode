4a9f3370cd7bd44ec6ee08ae9ede2c8cacf80442
Anthony Holloway <avholloway@gmail.com>
2021-01-13 02:35:50 +0000

    day 12 complete

--- day12.py
+++ day12.py
@@ -1,10 +1,42 @@
-################### Scope ####################
+logo = """
 
-enemies = 1
+  ██████  ██▓ ███▄    █  ██▓  ██████ ▄▄▄█████▓▓█████  ██▀███  
+▒██    ▒ ▓██▒ ██ ▀█   █ ▓██▒▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒
+░ ▓██▄   ▒██▒▓██  ▀█ ██▒▒██▒░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒
+  ▒   ██▒░██░▓██▒  ▐▌██▒░██░  ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  
+▒██████▒▒░██░▒██░   ▓██░░██░▒██████▒▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒
+▒ ▒▓▒ ▒ ░░▓  ░ ▒░   ▒ ▒ ░▓  ▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░
+░ ░▒  ░ ░ ▒ ░░ ░░   ░ ▒░ ▒ ░░ ░▒  ░ ░    ░     ░ ░  ░  ░▒ ░ ▒░
+░  ░  ░   ▒ ░   ░   ░ ░  ▒ ░░  ░  ░    ░         ░     ░░   ░ 
+      ░   ░           ░  ░        ░              ░  ░   ░     
+                                                              
 
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+"""
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+print(logo)
+
+from random import randint
+target = randint(1, 1_000_000)
+
+print("sinister is thinking of a number between 1 and 1,000,000.\n")
+difficulty = input("hard or easy: ")
+attempts = 10 if difficulty == "easy" else 5
+print(f"\nyou will have {attempts} attemps...let's go...\n")
+attempt = 1
+
+guessed = False
+while attempts > attempt:
+    attempt += 1
+    guess = int(input("guess: "))
+    if guess == target:
+        guessed = True
+        break
+    elif guess < target:
+        print(f"{guess} is too low")
+    else:
+        print(f"{guess} is too high")
+
+if guessed:
+    print("you guessed it!")
+else:
+    print(f"you failed it! it was {target}!")
