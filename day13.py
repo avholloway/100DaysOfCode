3c2386e47fb260218b8636c250fa9bb9ce9de428
Anthony Holloway <avholloway@gmail.com>
2021-01-13 21:04:09 +0000

    j/k now day 13 is complete

--- day13.py
+++ day13.py
@@ -70,12 +70,13 @@ def two():
 
 # 13.3
 # -----------------------------------------------------------------------------
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+def three():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
