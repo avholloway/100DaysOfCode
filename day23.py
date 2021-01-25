e91ad68274b2ac7f868328e8244f136c3dc3b0d3
avholloway <avholloway@gmail.com>
2021-01-24 21:47:11 -0600

    day 23 complete

--- day23.py
+++ day23.py
@@ -8,29 +8,72 @@ MOVE_INCREMENT = 10
 class CarManager():
     def __init__(self):
         self.level = 1
-        self.fleet = []
+        self.fleet = {}
+        self.lanes = range(200, -240, -40)
+        for lane in self.lanes:
+            self.fleet[lane] = []
         self.create_fleet()
-        self.lanes = range(240, -240, -40)
+        self.running = True
         
+    def tick(self):
+        if self.running:
+            for lane, cars in self.fleet.items():
+                for car in cars:
+                    if car.xcor() > -200:
+                        car.forward(5 + (self.level * 1.5))
+                        if player1.ycor() + 10 > car.ycor() - 10:
+                            if player1.xcor() + 10 > car.xcor() - 20:
+                                if player1.xcor() - 10 < car.xcor() + 20:
+                                    self.running = False
+                                    scoreboard.game_over()
+                    else:
+                        car.setup()
+                        car.sety(lane)
+                        self.lane_position(car, lane)
+                
     def create_fleet(self):
-        for _ in range(20):
-            self.add_car()
+        for lane in self.fleet.keys():
+            for _ in range(4):
+                car = Car()
+                self.assign_lane(car, lane)
+                self.lane_position(car, lane)
+                
+    def assign_lane(self, car, lane):
+        car.sety(lane)
+        self.fleet[lane].append(car)
         
-    def add_car(self):
-        car = Turtle()
-        car.penup()
-        car.shape("square")
-        car.shapesize(stretch_len=2)
-        car.color(choice(COLORS))
-        car.goto(200, choice(self.lanes))
-        self.fleet.append(car)
+    def lane_position(self, car, lane):
+        if len(self.fleet[lane]) == 0:
+            car.setx(randint(280, 380))
+        else:
+            car.setx(self.last_in_lane(lane).xcor() + randint(150, 850))
+            
+    def last_in_lane(self, lane):
+        last = None
+        for car in self.fleet[lane]:
+            if not last or car.xcor() > last.xcor():
+                last = car
+        return last
         
     def level_up(self):
         self.level += 1
 
+class Car(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.setup()
+        
+    def setup(self):
+        self.reset()
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_len=2)
+        self.color(choice(COLORS))
+        self.setheading(180)
+        
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
+FINISH_LINE_Y = 240
 
 class Player(Turtle):
     def __init__(self):
@@ -70,6 +113,10 @@ class Scoreboard(Turtle):
     def update_screen(self):
         self.clear()
         self.write(f"Level {self.level}", False, "center", FONT)
+        
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", False, "center", FONT)
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -82,11 +129,11 @@ car_manager = CarManager()
 screen.onkey(player1.advance, "Up")
 screen.listen()
 
-
 def run():
-    
-    screen.update()
-    screen.ontimer(run, 100)
+    if car_manager.running:
+        car_manager.tick()
+        screen.update()
+        screen.ontimer(run, 100)
 
 run()
 screen.mainloop()
