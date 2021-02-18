54ea1811bc11a2c0a0f247e2b76561454c452eb6
Anthony Holloway <avholloway@gmail.com>
2021-02-17 23:43:02 -0600

    day 48 complete

--- day048/main.py
+++ day048/main.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "/Users/anhollow/Programming/Utilities/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("http://secure-retreat-92358.herokuapp.com")
 
-# test
+first_name = driver.find_element_by_name("fName")
+first_name.send_keys("Anthony")
+
+last_name = driver.find_element_by_name("lName")
+last_name.send_keys("Holloway")
+
+email = driver.find_element_by_name("email")
+email.send_keys("avholloway@gmail.com")
+
+submit = driver.find_element_by_css_selector("body > form > button")
+submit.click()
+
+# driver.quit()
