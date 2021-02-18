c3bcab160630156f935a4001647c685179d66bfa
Anthony Holloway <avholloway@gmail.com>
2021-02-18 10:06:51 -0600

    day 48 improvements

--- day048/main.py
+++ day048/main.py
@@ -1,20 +1,44 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 chrome_driver_path = "/Users/anhollow/Programming/Utilities/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("http://secure-retreat-92358.herokuapp.com")
+ignored_exceptions = (NoSuchElementException, StaleElementReferenceException)
 
-first_name = driver.find_element_by_name("fName")
-first_name.send_keys("Anthony")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-last_name = driver.find_element_by_name("lName")
-last_name.send_keys("Holloway")
+money = driver.find_element_by_id("money")
 
-email = driver.find_element_by_name("email")
-email.send_keys("avholloway@gmail.com")
+cookie = driver.find_element_by_id("cookie")
 
-submit = driver.find_element_by_css_selector("body > form > button")
-submit.click()
+item_ids = [
+    "buyTime machine",
+    "buyPortal",
+    "buyAlchemy lab",
+    "buyShipment",
+    "buyMine",
+    "buyFactory",
+    "buyGrandma",
+    "buyCursor",
+]
 
-# driver.quit()
+while True:
+
+    for i in range(10):
+        cookie.click()
+
+    for _, item_id in enumerate(item_ids):
+        item = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions).until(expected_conditions.presence_of_element_located((By.ID, item_id)))
+        if item.get_attribute('class') != "grayed":
+            print(f"\tbuying {item_id}!")
+            item.click()
+            break
+
+driver.quit()
