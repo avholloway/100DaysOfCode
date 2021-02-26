b2369a31f47e81811a34908f89ce81febf8f055d
Anthony Holloway <avholloway@gmail.com>
2021-02-26 17:05:41 -0600

    day 57 complete

--- day057/server.py
+++ day057/server.py
@@ -1,3 +1,4 @@
+import requests
 from datetime import datetime as dt
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -9,6 +10,41 @@ def hello_world():
         "index.html",
         current_year=current_year
     )
+
+
+@app.route("/guess/<string:name>")
+def guess(name):
+    resp = requests.get(f"https://api.agify.io/?name={name}")
+    resp.raise_for_status()
+    age = resp.json()["age"]
+    resp = requests.get(f"https://api.genderize.io/?name={name}")
+    resp.raise_for_status()
+    gender = resp.json()["gender"]
+    return render_template(
+        "guess.html",
+        name=name,
+        age=age,
+        gender=gender
+    )
+
+
+@app.route("/blog")
+def blog_listing():
+    resp = requests.get("https://api.npoint.io/5abcca6f4e39b4955965")
+    resp.raise_for_status()
+    data = resp.json()
+    return render_template("blog-listing.html", data=data)
+
+
+@app.route("/post/<int:id>")
+def get_post(id):
+    resp = requests.get("https://api.npoint.io/5abcca6f4e39b4955965")
+    resp.raise_for_status()
+    data = resp.json()
+    for post in data:
+        if post["id"] == id:
+            return render_template("post.html", post=post)
+    return "404 their big guy"
 
 if __name__ == "__main__":
     app.run(debug=True)
