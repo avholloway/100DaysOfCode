eb561114083f68715748a3b2847ccf8b74e4b3d4
Anthony Holloway <avholloway@gmail.com>
2021-03-17 23:48:08 -0500

    day 071 complete

--- day071/main.py
+++ day071/main.py
@@ -1,3 +1,9 @@
 import pandas as pd
-
+pd.options.display.float_format = '{:,.2f}'.format 
 df = pd.read_csv("salaries_by_college_major.csv")
+df = df.dropna()
+spread_col = df['Mid-Career 90th Percentile Salary'] - df['Mid-Career 10th Percentile Salary']
+df.insert(1, 'Spread', spread_col)
+
+low_risk = df.sort_values('Spread')
+high_risk = df.sort_values("Spread", ascending=False)
